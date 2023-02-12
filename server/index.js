052c988f0378ca4d7f006564336ddc86a76115ff
Nizamshan <nizamshan@gmail.com>
2023-02-12 09:23:35 +0530

    final

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(express.json());
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname,"./client/public")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/public/index.html"),
+    path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
